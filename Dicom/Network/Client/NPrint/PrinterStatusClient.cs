c481241e204ca3236c8e67dcdd124f9365905508
Mahesh <mdubey82@gmail.com>
2010-11-09 12:55:24 +0530

    Added Printer Status SCU.

---
+++ Dicom/Network/Client/NPrint/PrinterStatusClient.cs
@@ -0,0 +1,193 @@
+﻿// mDCM: A C# DICOM library
+//
+// Copyright (c) 2006-2008  Colby Dillion
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// Contributing Author:
+//    Mahesh Dubey (mdubey82@gmail.com)
+
+using System;
+using System.Runtime.Remoting.Messaging;
+using Dicom.Data;
+namespace Dicom.Network.Client.NPrint
+{
+    public delegate void PrinterStatusErrorCallback(Exception ex);
+
+    /// <summary>
+    /// Scu for getting printer status.
+    /// </summary>
+    /// <example>
+    /// <para>
+    /// <code>
+    ///  PrinterStatusClient printerStatusSCU = new PrinterStatusClient()
+    ///  printerStatusSCU.BeginGetPrinterStatus("localhost", 7104, DcmSocketType.TCP, new AsyncCallback(EndGetPrinterStatus), printerStatusSCU);
+    ///  private void EndGetPrinterStatus(IAsyncResult result)
+    ///   {
+    ///     PrinterStatusClient printerStatusSCU = (PrinterStatusClient)result.AsyncState;
+    ///     var printerStatusResult = printerStatusSCU.EndGetPrinterStatus(result);
+    ///     if (printerStatusResult != null)
+    ///     {
+    ///        MessageBox.Show(printerStatusResult.ToString(), "Printer Status", MessageBoxButtons.OK, MessageBoxIcon.Information);
+    ///     }
+    ///  }
+    /// </code>
+    /// </para>
+    /// </example>
+    public class PrinterStatusClient :DcmClientBase
+    {
+        #region Private Delegate,Field,Property
+        private delegate PrinterStatusIOD GetPrinterStatusCallback(string host, int port, DcmSocketType socketType);
+        private DcmDataset _results;
+
+        private PrinterStatusIOD PrinterStatusResult
+        {
+            get
+            {
+                if (_results != null)
+                {
+                    return new PrinterStatusIOD(_results);
+                }
+                return null;
+            }
+        }
+        #endregion
+
+        #region Public C'tor, Event
+        public PrinterStatusErrorCallback OnPrinterStatusError;
+
+        /// <summary>
+        /// Initialize the new instance of <see cref="PrinterStatusClient"/> class.
+        /// </summary>
+        public PrinterStatusClient()
+            : base()
+        {
+            LogID = "PrinterStatusScu";
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Established asynchronous connection with peer entity.
+        /// </summary>
+        /// <param name="host">IP address of peer entity</param>
+        /// <param name="port">Port of the peer entity</param>
+        /// <param name="socketType"></param>
+        /// <param name="callback"></param>
+        /// <param name="asyncState"></param>
+        /// <returns></returns>
+        public IAsyncResult BeginGetPrinterStatus(string host, int port, DcmSocketType socketType, AsyncCallback callback, object asyncState)
+        {
+
+            GetPrinterStatusCallback getPrinterStatusDelegate = new GetPrinterStatusCallback(this.GetPrinterStatusAsync);
+            return getPrinterStatusDelegate.BeginInvoke(host, port, socketType, callback, asyncState);
+
+        }
+        /// <summary>
+        /// End the asynchronous operation and retrive the result.
+        /// </summary>
+        /// <param name="ar"></param>
+        /// <returns>The status of the printer</returns>
+        public PrinterStatusIOD EndGetPrinterStatus(IAsyncResult ar)
+        {
+            GetPrinterStatusCallback getPrinterStatusDelegate = ((AsyncResult)ar).AsyncDelegate as GetPrinterStatusCallback;
+            if (getPrinterStatusDelegate != null)
+            {
+                return getPrinterStatusDelegate.EndInvoke(ar) as PrinterStatusIOD;
+
+            }
+            else
+                throw new InvalidOperationException("Asynchresult is null");
+        }
+        #endregion
+
+        #region Private Method
+        private PrinterStatusIOD GetPrinterStatusAsync(string host, int port, DcmSocketType socketType)
+        {
+            this._results = null;
+            Connect(host, port, socketType);
+            if (!base.Wait())
+            {
+                if (OnPrinterStatusError != null)
+                {
+                    OnPrinterStatusError(new Exception(ErrorMessage));
+                }
+                Close();
+            }
+
+            return PrinterStatusResult;
+
+        }
+        /// <summary>
+        /// Send the N-Get request to the print SCP asking for its status. 
+        /// </summary>
+        /// <param name="association"></param>
+        private void SendRequest()
+        {
+            byte pcid = Associate.FindAbstractSyntax(DicomUID.PrinterSOPClass);
+            var requestedSOPClass = Associate.GetAbstractSyntax(pcid);
+            var requstedSOPInstance = DicomUID.PrinterSOPInstance;
+            DicomTag[] attributes = PrinterStatusIOD.RequestAttributes();
+            SendNGetRequest(pcid, NextMessageID(), requestedSOPClass, requstedSOPInstance, attributes);
+
+        }
+        #endregion
+
+        #region Overridden Methods
+        /// <summary>
+        /// Send the association request to the peer entity.
+        /// </summary>
+        protected override void OnConnected()
+        {
+
+            DcmAssociate associate = new DcmAssociate()
+            {
+                CalledAE = this.CalledAE,
+                CallingAE = this.CallingAE,
+                MaximumPduLength = this.MaxPduSize,
+
+            };
+
+            byte pcid = associate.AddPresentationContext(DicomUID.PrinterSOPClass);
+            associate.AddTransferSyntax(pcid, DicomTransferSyntax.ExplicitVRLittleEndian);
+            associate.AddTransferSyntax(pcid, DicomTransferSyntax.ImplicitVRLittleEndian);
+            SendAssociateRequest(associate);
+        }
+
+        protected override void OnReceiveAssociateAccept(DcmAssociate association)
+        {
+
+            byte pcid = association.FindAbstractSyntax(DicomUID.PrinterSOPClass);
+            if (association.GetPresentationContextResult(pcid) == DcmPresContextResult.Accept)
+            {
+                SendRequest();
+            }
+            else//Is it necessary ??
+            {
+                var msg = Dicom.Utility.Format.AddSpaces(association.GetPresentationContextResult(pcid).ToString());
+                OnPrinterStatusError(new Exception(msg));
+                Close();
+            }
+        }
+
+        protected override void OnReceiveNGetResponse(byte presentationID, ushort messageIdRespondedTo, DicomUID affectedClass, DicomUID affectedInstance, DcmDataset dataset, DcmStatus status)
+        {
+            this._results = dataset;
+            SendReleaseRequest();
+
+        }
+        #endregion
+    }
+}
---
+++ Dicom/Network/Client/NPrint/PrinterStatusIOD.cs
@@ -0,0 +1,211 @@
+﻿// mDCM: A C# DICOM library
+//
+// Copyright (c) 2006-2008  Colby Dillion
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+// Contributing Author:
+//    Mahesh Dubey (mdubey82@gmail.com)
+
+using System;
+using Dicom.Data;
+namespace Dicom.Network.Client.NPrint
+{
+    /// <summary>
+    /// C.13.9 Printer Module
+    /// Table C.13-9
+    /// PRINTER MODULE ATTRIBUTES
+    /// </summary>
+    public class PrinterStatusIOD
+    {
+        #region Private Field
+        private DcmDataset _dataset = null;
+        private DateTime _defaultDateTime = DateTime.MaxValue ;
+        #endregion
+
+        #region C'tor
+        /// <summary>
+        /// Initialize the new instance of <see cref="PrinterStatusIOD"/> class.
+        /// </summary>
+        /// <param name="dataset"></param>
+        public PrinterStatusIOD(DcmDataset dataset)
+        {
+            _dataset = dataset;
+        }
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        ///Printer device status. Enumerated Values:
+        ///NORMAL
+        ///WARNING
+        ///FAILURE
+        /// </summary>
+        public PrinterStatus PrinterStatus
+        {
+            get
+            {
+                return (PrinterStatus)Enum.Parse(typeof(PrinterStatus), _dataset.GetString(DicomTags.PrinterStatus, "None"), true);
+
+            }
+
+        }
+
+        /// <summary>
+        ///Additional information about Printer Status(2110,0010).
+        ///Defined Terms when the Printer Status is equal to
+        ///NORMAL:
+        ///      NORMAL
+        ///See Section C.13.9.1 for Defined Terms when the
+        ///Printer Status is equal to WARNING or FAILURE
+        /// </summary>
+        public string PrinterStatusInfo
+        {
+            get
+            {
+                return _dataset.GetString(DicomTags.PrinterStatusInfo, string.Empty);
+            }
+
+        }
+
+        /// <summary>
+        /// User defined name identifying the printer.
+        /// </summary>
+        public string PrinterName
+        {
+            get
+            {
+                return _dataset.GetString(DicomTags.PrinterName, string.Empty);
+
+            }
+
+        }
+        /// <summary>
+        /// Manufacturer of the printer.
+        /// </summary>
+        public string Manufacturer
+        {
+            get
+            {
+                return _dataset.GetString(DicomTags.Manufacturer, string.Empty);
+
+            }
+
+        }
+
+        /// <summary>
+        /// Manufacturer's model number of the printer.
+        /// </summary>
+        public string ManufacturersModelName
+        {
+            get
+            {
+                return _dataset.GetString(DicomTags.ManufacturersModelName, string.Empty);
+
+            }
+
+        }
+
+        /// <summary>
+        /// Manufacturer's serial number of the printer.
+        /// </summary>
+        public string DeviceSerialNumber
+        {
+            get
+            {
+                return _dataset.GetString(DicomTags.DeviceSerialNumber, string.Empty);
+
+            }
+
+        }
+        /// <summary>
+        /// Manufacturer's designation of software version of the printer.
+        /// </summary>
+        public string SoftwareVersions
+        {
+            get
+            {
+                return _dataset.GetString(DicomTags.SoftwareVersions, string.Empty);
+
+            }
+
+        }
+        /// <summary>
+        /// Date when the printer was last calibrated.
+        /// </summary>
+        public DateTime? DateOfLastCalibration
+        {
+            get
+            {
+                var result = _dataset.GetDateTime(DicomTags.DateOfLastCalibration, 0, _defaultDateTime);
+                if (result == _defaultDateTime)
+                    return null;
+                else
+                    return result;
+
+            }
+            
+        }
+        #endregion
+
+        #region Internal Static Method
+        /// <summary>
+        /// Creates the array of attributes for N-Get request.
+        /// </summary>
+        /// <returns>Arrays of dicom tags</returns>
+        internal static DicomTag[] RequestAttributes()
+        {
+            DicomTag[] tags = new DicomTag[] { DicomTags.PrinterStatus, DicomTags.PrinterStatusInfo, 
+                                               DicomTags.PrinterName, DicomTags.Manufacturer, 
+                                               DicomTags.ManufacturersModelName, DicomTags.DeviceSerialNumber,
+                                               DicomTags.SoftwareVersions, DicomTags.DateOfLastCalibration, 
+                                               DicomTags.TimeOfLastCalibration };
+            return tags;
+        }
+        #endregion
+
+        #region Override Method
+        /// <summary>
+        /// Returns the result in string format.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return string.Format("DateOfLastCalibration : {0}\nDeviceSerialNumber : {1}\nManufacturer : {2}\nManufacturersModelName : {3}\nPrinterName : {4}\nPrinterStatusInfo : {5}\nSoftwareVersions : {6} ", this.DateOfLastCalibration, this.DeviceSerialNumber, this.Manufacturer, this.ManufacturersModelName, this.PrinterName, this.PrinterStatus, this.PrinterStatusInfo, this.SoftwareVersions);
+
+        }
+        #endregion
+    }
+    
+    #region Printer Status Enum
+    /// <summary>
+    ///Printer device status. Enumerated Values:
+    ///None : If the status is empty. 
+    ///NORMAL
+    ///WARNING
+    ///FAILURE
+    /// </summary>
+    public enum PrinterStatus
+    {
+        None,
+
+        Normal,
+
+        Warning,
+
+        Failure
+    }
+    #endregion
+}
